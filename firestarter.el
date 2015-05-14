a97b82d3f563e8d52c86768894f68b0c1093684c
Vasilij Schneidermann <v.schneidermann@gmail.com>
2015-05-15 01:18:26 +0200

    Bump version

--- firestarter.el
+++ firestarter.el
@@ -4,7 +4,7 @@
 
 ;; Author: Vasilij Schneidermann <v.schneidermann@gmail.com>
 ;; URL: https://github.com/wasamasa/firestarter
-;; Version: 0.1.2
+;; Version: 0.1.3
 ;; Keywords: convenience
 
 ;; This file is NOT part of GNU Emacs.
